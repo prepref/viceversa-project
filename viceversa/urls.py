88e10d6ff8c12c4008e7a9c7629d58b75301594f
Vasily <popov.to@mail.ru>
2023-02-05 00:09:06 +0500

    Reversed Text

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('reverse/', views.reverse)
+    path('reversed/', views.reverse, name = 'reverse')
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def home(request):
 	return render(request, 'home.html')
 def reverse(request):
-	return render(request, 'reverse.html')
+	user_text = request.GET['usertext']
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html',{'usertext':user_text, 'reversedtext':reversed_text})
